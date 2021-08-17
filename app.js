7fd47950a8ac2bee63b20d4c73305e46b4adccc1
Elton <eltonsa75@hotmail.com>
2021-08-17 11:23:51 -0300

    Porta da aplicação configurada

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 /* importar as configurações do servidor */
+var app = require('./config/server');
+
+/* parametrizar a porta de escuta */
+app.listfen(80, function(){
+    console.log('Servidor online');
+})
